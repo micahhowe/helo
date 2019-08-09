d68383705900ddf0e13360d0d5fdce1128aa7a57
micahhowe <micahhowe182@gmail.com>
2019-08-09 09:32:27 -0600

    server is up and running

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+//const session = require('express-session')
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+const ctrl = require('./controller.js')
+
+
+const app = express()
+//Make sure that your middleware (app.use) is above the endpoints so they can run first
+app.use(express.json())
+// app.use(session({
+//     secret: SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24 * 10
+//     }
+//   }))
+
+
+
+massive(CONNECTION_STRING).then(db => {
+  app.set('db', db)
+  app.listen(SERVER_PORT, () => console.log(`YEAH! Running on ${SERVER_PORT}`))
+})
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+//import {Link} from 'react-router-dom'
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <div className="Nav">
+        Nav
+      </div>
+    )
+  }
+}
