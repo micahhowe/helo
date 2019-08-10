3f74e19ce5c05fb11b5108359d7e08a4e0c3b9aa
micahhowe <micahhowe182@gmail.com>
2019-08-10 17:23:35 -0600

    the dashboard search is now saving hte input value on state

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react'
 //import {Link} from 'react-router-dom'
 
 export default class Dashboard extends Component {
+    constructor(props) {
+        super(props)
+            
+        this.state = {
+            searchValue: ''
+          }
+        }
+    handleChange(e) {
+        this.setState({ searchValue: e.target.value })
+    }
   render() {
+      console.log(this.state.searchValue)
     return (
       <div className="Dashboard">
         Dashboard
-        <input type="text"/>
+        <input onChange={e => this.handleChange(e)} type="text" placeholder="Search"/>
         <button>Search</button>
         <button>Reset</button>
         <h3>My Posts</h3>
